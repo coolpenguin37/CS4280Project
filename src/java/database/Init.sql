5417e685e60b3272fdb770f9e04bb4c2939f65d3
coolpenguin37 <coolpenguin37@gmail.com>
2016-03-20 15:01:27 +0800

    Add File

--- src/java/database/Init.sql
+++ src/java/database/Init.sql
@@ -1,7 +1,12 @@
 DELETE FROM [User];
+DBCC CHECKIDENT ([User], RESEED, 0)
 INSERT INTO [User] VALUES ('useralice', 'pwalice', 'alice', 'alice@cityu.edu.hk', '12345678', '1', '1');
 INSERT INTO [User] VALUES ('userbob', 'pwbob', 'bob', 'bob@cityu.edu.hk', '12345678', '0', '1');
 INSERT INTO [User] VALUES ('usereve', 'pweve', 'eve', 'eve@cityu.edu.hk', '12345678', '1', '2');
+SELECT * FROM [User];
 
-DELETE FROM [Hotel];
-INSERT 
+DELETE FROM [HotelInfo];
+DBCC CHECKIDENT ([HotelInfo], RESEED, 0)
+INSERT INTO [HotelInfo] VALUES ('Smirnoff', 'Moscow', 0, 4);
+INSERT INTO [HotelInfo] VALUES ('ICON', 'Hong Kong', 1, 5);
+INSERT INTO [HotelInfo] VALUES ('Park Hyatt', 'Tokyo', 1, 5);
