09fe3a0d2d04094834b588d964fba8753a67ac71
yduan7 <yduan7@XNT08.cs.cityu.edu.hk>
2016-03-21 22:19:28 +0800

    Log: Add createAccount (ver 0.1) servlet for parsing input, insert user and return results; Add newAccount.html (ver 0.1) for users to input data (note: no instant username duplication checking is implemented yet); Update User.java to delete the userID parameter in second constructor; Update web.xml to include servlet mapping for createAccount;

---
+++ src/java/servlets/createAccount.java
@@ -0,0 +1,128 @@
+package servlets;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.lang.Object;
+import user.*;
+
+/**
+ *
+ * @author yduan7
+ */
+public class createAccount extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet createAccount</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            String username=request.getParameter("username");
+            String password=request.getParameter("password");
+            String name, telephone, email;
+            int isSubscribed;
+            if (request.getParameter("name")==null){
+                name="";
+            }
+            else{
+                name=request.getParameter("name");
+            }
+            if (request.getParameter("telephone")==null){
+                telephone="";
+            }
+            else{
+                telephone=request.getParameter("telephone");
+            }
+            if (request.getParameter("email")==null){
+                email="";
+            }
+            else{
+                email=request.getParameter("email");
+            }
+            
+            if (request.getParameter("subscribe")==null){
+                isSubscribed=0;
+            }
+            else {
+                isSubscribed=1;
+            }
+            
+            User u=new User(username,password,name,email,telephone,isSubscribed,1);
+            if (u.insertToDatabase()) {
+            out.println("<p>Success!</p>");}
+            else{
+            out.println("<p>Failed...</p>");} 
+            out.println("</body>");
+            out.println("</html>");
+        } finally {
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
