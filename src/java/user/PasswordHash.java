fcea67282250ea36850af4075cdb3eb7a17e2c85
alan97 <yanlind@andrew.cmu.edu>
2016-03-23 11:48:46 +0800

    1. Add loginException.java ; 2. Update newAccount.html; 3. createAccount java now supports password hash; 4. Add loginAccount.html (may be depleted as a jsp may be used instead); 5. Update web.xml; 6. Add PasswordHash.jav (using SHA-256)

---
+++ src/java/user/PasswordHash.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package user;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+/**
+ *
+ * @author yduan7
+ */
+public class PasswordHash {
+    public static String hash(String password) throws NoSuchAlgorithmException{
+        
+        MessageDigest md= MessageDigest.getInstance("SHA-256");
+        md.update(password.getBytes());
+        String encryptedPassword=new String(md.digest());
+        return encryptedPassword;}
+}
