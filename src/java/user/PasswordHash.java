8a25ad856a6a3453a3bb992fc4d13285315d46d8
coolpenguin37 <coolpenguin37@gmail.com>
2016-03-24 21:48:44 +0800

    Fix the bug the user cannot login.

--- src/java/user/PasswordHash.java
+++ src/java/user/PasswordHash.java
@@ -16,7 +16,13 @@ public class PasswordHash {
         
         MessageDigest md= MessageDigest.getInstance("SHA-256");
         md.update(password.getBytes());
-        String encryptedPassword=new String(md.digest());
-        return encryptedPassword;
+        byte[] bytes= md.digest();
+        StringBuilder sb = new StringBuilder(2 * bytes.length);
+        for (byte b : bytes) {
+            sb.append("0123456789ABCDEF".charAt((b & 0xF0) >> 4));
+            sb.append("0123456789ABCDEF".charAt((b & 0x0F)));
+        }
+        String hex = sb.toString();
+        return hex;
     }
 }
